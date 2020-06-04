abb1cb88a2e64099aec1aa4d41fb8ca732d81502
Bogdan Bzenko <bogdan.bzenko@spd-group.com>
2020-06-04 23:46:20 +0300

    Updates

---
+++ precache-manifest.d0db220884e7a8b61fe47326e9c2f481.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ff4cefe8122ba99a3186b99df33bff",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e2cf8d1f4db9302846ff",
+    "url": "/weather-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "835a2fcf8e6122ef79b2",
+    "url": "/weather-app/static/css/main.ff5dd491.chunk.css"
+  },
+  {
+    "revision": "e2cf8d1f4db9302846ff",
+    "url": "/weather-app/static/js/2.412b5f02.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/weather-app/static/js/2.412b5f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "835a2fcf8e6122ef79b2",
+    "url": "/weather-app/static/js/main.ddafd78f.chunk.js"
+  },
+  {
+    "revision": "7d28e1659337914de9cc",
+    "url": "/weather-app/static/js/runtime-main.1bf6a0e2.js"
+  }
+]);
